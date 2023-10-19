22396bacb3d2900a4051d5e578c375818e43865b
Asiri shamika <107676071+asirishamika@users.noreply.github.com>
2023-10-19 15:24:57 +0530

    Build the Color Mode Switch

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ const App = () => {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
+        <GridItem area="aside" bg="">
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main" bg="">
         Main
       </GridItem>
     </Grid>
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,15 +1,17 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import logo from "../assets/viaraLogo.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   /**Hstack is chakra ui horizontal stack
    * image also should import using chakra ui
    **/
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding="20px">
       <Image src={logo} boxSize="60px" />
       <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
